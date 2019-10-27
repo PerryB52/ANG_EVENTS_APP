f9e8a7eda747206e9f5672416dadc8332025f41d
Alexandru Pereteanu <alexandrup@softinfo.ro>
2019-10-27 11:07:54 +0200

    s10 v7 sorting data

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -8,12 +8,14 @@ import { ISession } from '../shared'
 export class SessionListComponent implements OnChanges {
     @Input() sessions: ISession[]
     @Input() filterBy: string
+    @Input() sortBy: string
     visibleSessions: ISession[] = [];
 
     //this will be called anytime an input variable is changed, will use this for filtering
     ngOnChanges() {
         if (this.sessions) {
             this.filterSessions(this.filterBy);
+            this.sortBy === 'name' ? this.visibleSessions.sort(sortByNameAsc) : this.visibleSessions.sort(sortByVotesDesc)
         }
     }
 
@@ -26,4 +28,14 @@ export class SessionListComponent implements OnChanges {
             })
         }
     }
+}
+
+function sortByNameAsc(s1: ISession, s2: ISession){
+    if (s1.name > s2.name) return 1
+    else if (s1.name === s2.name) return 0
+    else return -1
+}
+
+function sortByVotesDesc(s1: ISession, s2: ISession) {
+    return s2.voters.length - s1.voters.length
 }
