ae5ebff25e81bd9841c950edb15239e36ea66e36
Alexandru Pereteanu <alexandrup@softinfo.ro>
2019-11-07 18:54:47 +0200

    s12 v4 creating a modal component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,13 @@ import {
 
 import { EventsAppComponent } from './events-app.component';
 import { NavBarComponent } from './nav/nav-bar.component'
-import { JQ_TOKEN, TOASTR_TOKEN, Toastr, CollapsibleWellComponent } from './common/index';
+import {
+  JQ_TOKEN,
+  TOASTR_TOKEN,
+  Toastr,
+  CollapsibleWellComponent,
+  SimpleModalComponent
+} from './common/index';
 import { appRoutes } from './routes';
 import { Error404Component } from './errors/404.component';
 import { AuthService } from './user/auth.service';
@@ -50,6 +56,7 @@ let jQuery = window['$'];
   providers: [
     EventService,
     { provide: TOASTR_TOKEN, useValue: toastr },
+    { provide: JQ_TOKEN, useValue: jQuery },
     EventRouteActivator,
     EventListResolver,
     AuthService, //providers are shared across modules, declarations and imports are NOT
