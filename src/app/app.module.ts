ec88afc2d40238a69bedf7d3b4907af2d2dec923
Alexandru Pereteanu <alexandrup@softinfo.ro>
2019-11-12 23:01:41 +0200

    s14 v2 preparing to store data on the server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http'
 
 import {
   EventsListComponent,
@@ -43,7 +44,8 @@ let jQuery = window['$'];
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   declarations: [
     EventsAppComponent,
